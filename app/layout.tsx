5ae4052010e26ebe2ac536ab0a8eb6fa4fd700ac
jaymaxx6702-arch <jaymaxx6702@gmail.com>
2026-09-15 14:45:58 +0530

    Fix YashFlow PWA manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,30 @@ export const metadata: Metadata = {
   title: "YashFlow",
   description: "Yash Laser Work Management",
   applicationName: "YashFlow",
+
+  // Explicit PWA manifest link
+  manifest: "/manifest.webmanifest",
+
   icons: {
-    icon: "/icon-192.png",
+    icon: [
+      {
+        url: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
     apple: "/icon-192.png",
+  },
+
+  appleWebApp: {
+    capable: true,
+    title: "YashFlow",
+    statusBarStyle: "default",
   },
 };
 
@@ -30,9 +51,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-  <ServiceWorkerRegister />
-  {children}
-</body>
+        <ServiceWorkerRegister />
+        {children}
+      </body>
     </html>
   );
 }
