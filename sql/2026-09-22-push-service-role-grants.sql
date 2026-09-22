5e403fde2276bbd80b5028ccdb7f83955140c0d6
Jay Maxx <jaymaxx6702@gmail.com>
2026-09-22 15:17:07 +0530

    Grant service role native FCM token access

--- sql/2026-09-22-push-service-role-grants.sql
+++ sql/2026-09-22-push-service-role-grants.sql
@@ -18,6 +18,14 @@ grant select, insert, update
 on table public.push_subscriptions
 to service_role;
 
+do $native_push_grants$
+begin
+  if to_regclass('public.native_push_tokens') is not null then
+    execute 'grant select, insert, update on table public.native_push_tokens to service_role';
+  end if;
+end
+$native_push_grants$;
+
 grant select, update
 on table public.push_outbox
 to service_role;
